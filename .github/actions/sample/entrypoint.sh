e1f6acb7cf01877834ebf69d90ccda9554743a6d
aw <aw@pionix.de>
2022-06-10 19:08:26 +0200

    some changes

--- .github/actions/sample/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -l
-
-clang-format --dry-run --style=file /github/workspace/src/test.cpp
-
-#echo "Hello $1"
-#time=$(date)
-#echo "::set-output name=time::$time"
-
--- docker/linter/entrypoint.sh
+++ docker/linter/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
-clang-format --dry-run --style=file /github/workspace/src/test.cpp
-
+# clang-format --dry-run --style=file /github/workspace/src/test.cpp
+clang-format --dry-run --style=file src/test.cpp | reviewdog -efm="%f:%l:%c: warning: %m" -reporter=github-check
 #echo "Hello $1"
 #time=$(date)
 #echo "::set-output name=time::$time"
