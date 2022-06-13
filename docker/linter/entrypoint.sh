fff8733b0f448eb0225fa562ca5899cb24a31001
aw <aw@pionix.de>
2022-06-13 23:28:29 +0200

    Changed to alpine image
    Signed-off-by: aw <aw@pionix.de>

--- docker/linter/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -l
-
-# clang-format --dry-run --style=file /github/workspace/src/test.cpp
-clang-format --dry-run --style=file src/test.cpp | reviewdog -efm="%f:%l:%c: warning: %m" -reporter=github-check
-#echo "Hello $1"
-#time=$(date)
-#echo "::set-output name=time::$time"
-
