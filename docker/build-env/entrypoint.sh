e467ad894e1f3772f52ee07f4bbe54d485bb9e4e
aw <aw@pionix.de>
2023-05-10 14:35:26 +0200

    Rework
    Signed-off-by: aw <aw@pionix.de>

---
+++ docker/build-env/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+setup_cache(){
+    export CPM_SOURCE_CACHE=${EXT_MOUNT}/cache/cpm
+    export CCACHE_DIR=${EXT_MOUNT}/cache/ccache
+
+    maven_cache_dir=${EXT_MOUNT}/cache/m2-repo
+
+    # we copy the settings file each time in case the $HOME folder changes
+    mkdir -p $HOME/.m2 || true
+    cp $ASSETS_PATH/maven-settings.xml $HOME/.m2/settings.xml
+    sed -i 's@MAVEN_CACHE_REPOSITORY@'"${maven_cache_dir}"'@' $HOME/.m2/settings.xml
+}
+
+run_init(){
+    init_script=$EXT_MOUNT/scripts/init.sh
+
+    if [ -f "${init_script}" ]; then
+        exec ${init_script}
+    else
+        printf "No ${init_script} available, executing sh\\n"
+        exec /bin/sh
+    fi
+}
+
+setup_cache
+run_init
+
